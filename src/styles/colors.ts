6540e2acceed4decc068b8150f0f1236d000a472
Renato Souza <natoicm@gmail.com>
2023-06-03 22:14:06 -0300

    style: add pallete colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,16 @@
+export const COLORS = {
+  green: '#1E9400',
+  greenLight: '#A5FF32',
+  
+  blueLight: '#12C2E9',
+  blueDark: '#142995',
+
+  gray: '#BBBBBB',
+  grayLight: '#EEEEEE',
+  grayDark: '#3F3F3F',
+
+  white: '#FFFFFF',
+  black: '#111111',
+  red: '#FF0000',
+  yellow: '#FFB800'
+}
