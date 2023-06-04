3c2f1ab47fd26661aec05a838a97aa808a7a707f
Renato Souza <natoicm@gmail.com>
2023-06-03 22:22:50 -0300

    chore: add PT Sans fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { PTSans_400Regular } from '@expo-google-fonts/pt-sans'
+import { PTSansCaption_400Regular } from '@expo-google-fonts/pt-sans-caption'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    PTSans_400Regular,
+    PTSansCaption_400Regular
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
